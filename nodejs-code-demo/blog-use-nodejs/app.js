1b646af625564890c3f68ead2ce1317a6719e87c
鲍佳玮 <bjw_0627@163.com>
2020-04-11 21:50:45 +0800

    所有接口造假数据，封装获取postData的promise方法

--- nodejs-code-demo/blog-use-nodejs/app.js
+++ nodejs-code-demo/blog-use-nodejs/app.js
@@ -1,6 +1,32 @@
 const querystring = require('querystring')
 const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
+const getPostData = (req) => {
+  const promise = new Promise((resolve, reject) => {
+    if (req.method !== 'POST') {
+      resolve({})
+      return
+    }
+    if (req.headers['content-type'] !== 'application/json') {
+      resolve({})
+      return
+    }
+    let postData = ''
+    req.on('data', chunk => {
+      postData += chunk.toString()
+    })
+    req.on('end', () => {
+      if (!postData) {
+        resolve({})
+        return
+      }
+      resolve(
+        JSON.parse(postData)
+      )
+    })
+  })
+  return promise
+}
 const serverHandle = (req, res) => {
   // 设置返回格式 JSON
   res.setHeader('Content-type', 'application/json')
@@ -11,28 +37,32 @@ const serverHandle = (req, res) => {
   // 解析query
   req.query = querystring.parse(url.split('?')[1])
 
-  // 处理blog路由
-  const blogData = handleBlogRouter(req, res)
-  if (blogData) {
-    res.end(
-      JSON.stringify(blogData)
-    )
-    return
-  }
+  getPostData(req).then(postData => {
+    req.body = postData
 
-  // 处理user路由
-  const userData = handleUserRouter(req, res)
-  if (userData) {
-    res.end(
-      JSON.stringify(userData)
-    )
-    return
-  }
+    // 处理blog路由
+    const blogData = handleBlogRouter(req, res)
+    if (blogData) {
+      res.end(
+        JSON.stringify(blogData)
+      )
+      return
+    }
 
-  //未命中路由，返回404
-  res.writeHead(404, { "Content-type": "text/plain" })
-  res.write("404 Not Found\n")
-  res.end()
+    // 处理user路由
+    const userData = handleUserRouter(req, res)
+    if (userData) {
+      res.end(
+        JSON.stringify(userData)
+      )
+      return
+    }
+
+    //未命中路由，返回404
+    res.writeHead(404, { "Content-type": "text/plain" })
+    res.write("404 Not Found\n")
+    res.end()
+  })
 }
 
 module.exports = serverHandle
--- nodejs-code-demo/blog-use-nodejs/src/controller/blog.js
+++ nodejs-code-demo/blog-use-nodejs/src/controller/blog.js
@@ -1,33 +1,31 @@
 const getList = (author, keyword) => {
-  return [
-    {
-      id: 1,
-      title: '标题A',
-      content: '内容A',
-      createTime: 1546610491112,
-      author: 'zhangsan'
-    },
-    {
-      id: 2,
-      title: '标题B',
-      content: '内容B',
-      createTime: 1546610524373,
-      author: 'lisi'
-    }
-  ]
 }
 
 const getDetail = (id) => {
+}
+
+const newBlog = (blogData = {}) => {
+  // blogData 是一个博客对象，包含title content属性
   return {
-    id: 1,
-    title: '标题A',
-    content: '内容A',
-    createTime: 1546610491112,
-    author: 'zhangsan'
+    id: 3 //表示新建博客，插入到数据表里的id
   }
+}
+
+const updateBlog = (id, blogData = {}) => {
+  // id 就是要更新博客的id
+  // blogData 是一个博客对象，包含title content属性
+  return true
+}
+
+const delBlog = (id) => {
+  // id 就是要更新博客的id
+  return true
 }
 
 module.exports = {
   getList,
-  getDetail
+  getDetail,
+  newBlog,
+  updateBlog,
+  delBlog
 }
