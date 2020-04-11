70ebb792031f727635bcf489482b2c525b2d2dcb
鲍佳玮 <bjw_0627@163.com>
2020-04-11 21:04:54 +0800

    假数据，增加controller和model目录

--- nodejs-code-demo/blog-use-nodejs/app.js
+++ nodejs-code-demo/blog-use-nodejs/app.js
@@ -1,8 +1,15 @@
+const querystring = require('querystring')
 const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
 const serverHandle = (req, res) => {
   // 设置返回格式 JSON
   res.setHeader('Content-type', 'application/json')
+  // 获取path
+  const url = req.url
+  req.path = url.split('?')[0]
+
+  // 解析query
+  req.query = querystring.parse(url.split('?')[1])
 
   // 处理blog路由
   const blogData = handleBlogRouter(req, res)
@@ -23,7 +30,7 @@ const serverHandle = (req, res) => {
   }
 
   //未命中路由，返回404
-  res.writeHead(404, { "Content-type": "text/plain"})
+  res.writeHead(404, { "Content-type": "text/plain" })
   res.write("404 Not Found\n")
   res.end()
 }
