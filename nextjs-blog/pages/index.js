5a532b163cf829f21eb45017ca9ebc036d1c0358
鲍佳玮 <bjw_0627@163.com>
2020-04-23 18:11:46 +0800

    根据文档，构建博客首页

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import Alert from '../components/alert/alert'
+import utilStyles from '../styles/utils.module.css'
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Recording front-end development learning</p>
+        <Alert type="success">测试alert</Alert>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  )
+}
