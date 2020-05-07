c2fe76dd5931cc82d94e837979b1124e92394548
鲍佳玮 <bjw_0627@163.com>
2020-05-07 17:18:41 +0800

    接入koa-morgan

--- nodejs-code-demo/blog-use-koa2/app.js
+++ nodejs-code-demo/blog-use-koa2/app.js
@@ -7,6 +7,9 @@ const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 const session = require('koa-generic-session')
 const redisStore = require('koa-redis')
+const path = require('path')
+const fs = require('fs')
+const morgan = require('koa-morgan')
 
 const { REDIS_CONF } = require('./conf/db')
 
@@ -37,6 +40,19 @@ app.use(async (ctx, next) => {
   const ms = new Date() - start
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
+
+const env = process.env.NODE_ENV
+if(env !== 'production') {
+  app.use(morgan('dev'));
+} else {
+  const logFileName = path.join(__dirname, 'logs', 'access.log')
+  const writeStream = fs.createWriteStream(logFileName, {
+    flags: 'a'
+  })
+  app.use(morgan('combined', {
+    stream: writeStream
+  }));
+}
 
 app.keys = ['bjw#zs123']
 app.use(session({
