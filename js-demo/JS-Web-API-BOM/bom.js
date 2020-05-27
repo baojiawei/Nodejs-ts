2c93dc2e7b0168998d86e59c40f4a5800af8cd40
鲍佳玮 <bjw_0627@163.com>
2020-05-27 17:35:40 +0800

    Bom操作

---
+++ js-demo/JS-Web-API-BOM/bom.js
@@ -0,0 +1,19 @@
+// navigator
+const ua = navigator.userAgent
+const isChrome = ua.indexOf('Chrome')
+console.log(isChrome)
+
+// screen
+console.log(screen.width)
+console.log(screen.height)
+
+// location
+console.log(location.protocol)
+console.log(location.host)
+console.log(location.search)
+console.log(location.hash)
+console.log(location.pathname)
+
+// history
+history.back()
+history.forward()
